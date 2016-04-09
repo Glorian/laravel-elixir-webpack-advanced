333ff1645e6df79d1bd4e88fd7ab6a5c915bd881
Glorian <rjab4ik@gmail.com>
2016-04-09 11:32:35 +0300

    Fixed couple bugs in versioning system

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
 "use strict";
 
+/**
+ * Require main dependencies
+ */
 const
     _ = require('lodash'),
     path = require('path'),
-    gulp = require('gulp'),
     webpack = require('webpack'),
     elixir = require('laravel-elixir'),
     webpack_config = require('./conf/webpack');
 
+/**
+ * Helpers
+ */
 const
     $ = elixir.Plugins,
     config = elixir.config,
     taskName = 'webpack';
 
-let prepGulpPaths = require('./lib/GulpPaths'),
+/**
+ * Built-in modules
+ */
+const
+    prepGulpPaths = require('./lib/GulpPaths'),
     isVersioning = require('./lib/IsVersioning'),
     prepareEntry = require('./lib/EntryPaths'),
     saveFiles = require('./lib/SaveFiles'),
     isWatch = require('./lib/IsWatch');
 
+/**
+ * Webpack spec
+ */
 elixir.extend(taskName, function (src, options, globalVars) {
     let paths = prepGulpPaths(src),
         entry = prepareEntry(src);
 
+    /**
+     * In next major release this will be removed
+     * TODO mark as deprecated
+     */
     if (_.isPlainObject(globalVars)) {
         webpack_config.plugins.push(new webpack.ProvidePlugin(globalVars));
     }
 
-    options = _.mergeWith(webpack_config, options, {entry, watch: isWatch()}, (objValue, srcValue) => {
-        if (_.isArray(objValue)) {
-            return objValue.concat(srcValue);
+    // Merge options
+    options = _.mergeWith(
+        webpack_config,
+        options,
+        {entry, watch: isWatch()},
+        (objValue, srcValue) => {
+            if (_.isArray(objValue)) {
+                return objValue.concat(srcValue);
+            }
         }
-    });
+    );
 
     if (isVersioning()) {
         options.output.publicPath = options.output.publicPath
@@ -39,10 +61,16 @@ elixir.extend(taskName, function (src, options, globalVars) {
             // Add leading slash if missing
             .replace(/^\/?/, '/')
 
-            // prepend build folder to public path
-            .replace(/^/, `/${config.versioning.buildFolder}`);
+            // insert build folder before js output
+            .replace(
+                new RegExp(config.js.outputFolder),
+                `${config.versioning.buildFolder}/${config.js.outputFolder}`
+            );
     }
 
+    /**
+     * Webpack task
+     */
     new elixir.Task(taskName, function () {
         this.log(paths.src, saveFiles(src, paths));
 
